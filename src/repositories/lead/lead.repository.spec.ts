b8c5f6448217865bf02e5df2d558d9c5768a4bab
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-07-08 12:47:11 -0300

    fix: Retirando uuid do repositório

--- src/repositories/lead/lead.repository.spec.ts
+++ src/repositories/lead/lead.repository.spec.ts
@@ -15,21 +15,19 @@ describe('Lead Repository Unit Test', () => {
         await prisma.$disconnect();
     });
 
-    afterEach(async () => {
-        await prisma.lead.deleteMany();
-    })
 
     it('create should a new lead', async () => {
         const newLead: LeadEntity = {
-            id: "46f912b9-e6b6-44a9-827f-660bb9c252e2",
+            id: "teste1",
             name: "joao",
             email: 'joao@gmail.com',
         };
 
         const createdLead: LeadEntity = await leadRepository.create(newLead);
 
-        expect(createdLead.id).toBe("46f912b9-e6b6-44a9-827f-660bb9c252e2");
+        expect(createdLead.id).toBe("teste1");
         expect(createdLead.name).toBe('joao');
         expect(createdLead.email).toBe("joao@gmail.com");
     })
+
 })
--- src/repositories/lead/lead.repository.ts
+++ src/repositories/lead/lead.repository.ts
@@ -1,17 +1,14 @@
 import {PrismaClient } from '@prisma/client';
 import { LeadEntity } from '../../entities/lead.entity';
-import { v4 as uuidv4 } from 'uuid';
 
 
 const prisma = new PrismaClient();
 
 export class LeadRepository{
     async create(data: LeadEntity): Promise<LeadEntity>{
-        const postId = uuidv4();
         const {...leadData} = data;
         const lead = await prisma.lead.create({
             data: {
-                id: postId,
                 ...leadData,
             },
         });
